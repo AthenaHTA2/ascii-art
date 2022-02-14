1f3328580462751d279d2f2b158b931ea03f7c3b
AthenaHTA2 <sedmakh2@gmail.com>
2022-02-14 15:17:23 +0000

    In progress: draft main file by Helena

--- example_HTA.go
+++ example_HTA.go
@@ -16,9 +16,9 @@ func main() {
 		inputToSlice = append(inputToSlice, userInput[i]) // put user input into a string slice
 	}
 
-	var bannerSlices []string //a slice of strings containing the 95 images from standard.txt file
-	counter := 0
-	//var strLine []string
+	var bannerSlices []string // a slice of strings containing the 95 images from standard.txt file
+
+	// var strLine []string
 
 	banner, err := os.Open("standard.txt")
 	if err != nil {
@@ -28,22 +28,26 @@ func main() {
 	defer banner.Close() // the file descriptor is closed at the end of the main function
 
 	scanner := bufio.NewScanner(banner) // make new Scanner
-	for scanner.Scan() {                //scan the 'standard.txt' file
-		line := scanner.Text()                   // store 'standard.txt' into the variable 'line'
-		bannerSlices = strings.Split(line, "\n") //separate 'standard.txt' after each instance of a new line and populate a slice of strings
-		fmt.Println("length of bannerSlices: ", counter, bannerSlices)
-		counter = counter + 1
-		//fmt.Println(bannerSlices)
-		//for j := 10; j <= 35; j++ {
-		//	fmt.Println("printing line by line", bannerSlices[j])
-		//}
+	for scanner.Scan() {                // scan the 'standard.txt' file
+		line := scanner.Text() // store 'standard.txt' into the variable 'line'
+		// fmt.Println("Line: ", line)
+		bannerSlices = append(bannerSlices, line+"\n")
+		// separate 'standard.txt' after each instance of a new line and populate a slice of strings
+		// fmt.Println("length of bannerSlices: ", counter, bannerSlices)
+
 	}
+	var bannerArray []string
+	fmt.Println(bannerSlices[8:16])
+
+	for i := 0; i < len(bannerSlices); i += 9 {
+		bannerArray = append(bannerArray, strings.Join(bannerSlices[i:i+9], ""))
+	}
+	fmt.Println(bannerArray[3])
 	if err := scanner.Err(); err != nil {
 		fmt.Println(err)
 	}
 
-	//theMap := make(map[string]rune) // matching 95 images from 'standard.txt' to 95 runes in the ascii table, from dec 32 to dec 126.
+	// theMap := make(map[string]rune) // matching 95 images from 'standard.txt' to 95 runes in the ascii table, from dec 32 to dec 126.
 
-	//m[bannerSlice[0]] = rune(31)
-
+	// m[bannerSlice[0]] = rune(31)
 }
