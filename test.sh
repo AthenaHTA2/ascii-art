d821360dad082a8f90652922f55aac0fd3735564
Nathalie <rs.nathalie@yahoo.com>
2022-02-15 13:28:08 +0000

    trial

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+go run example_Bunmi.go "hello" | cat -e
+go run example_Bunmi.go "HELLO" | cat -e
+go run example_Bunmi.go "HeLlo HuMaN" | cat -e
+go run example_Bunmi.go "1Hello 2There" | cat -e
+go run example_Bunmi.go "Hello\nThere" | cat -e
+go run example_Bunmi.go "{hello & There #}" | cat -e
+go run example_Bunmi.go "hello There 1 to 2!" | cat -e
+go run example_Bunmi.go "MaD3IrA&LiSboN" | cat -e
+go run example_Bunmi.go "1a\"#FdwHywR&/()=" | cat -e
+go run example_Bunmi.go "{|}~" | cat -e
+go run example_Bunmi.go "[\]^_ 'a" | cat -e
+go run example_Bunmi.go "RGB" | cat -e
+go run example_Bunmi.go ":;<=>?@" | cat -e
+go run example_Bunmi.go "\!\" #$%&'()*+,-./" | cat -e
+go run example_Bunmi.go "ABCDEFGHIJKLMNOPQRSTUVWXYZ" | cat -e
+go run example_Bunmi.go "abcdefghijklmnopqrstuvwxyz" | cat -e
